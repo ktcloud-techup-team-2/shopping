01b07ae0b689f3be4f7cf93bceff4fbb7ea7fda3
son_mac <smk2591@naver.com>
2025-11-20 16:49:33 +0900

    feat: DeliveryAddressRepository 구현

---
+++ src/main/java/com/kt/repository/delivery/DeliveryAddressRepository.java
@@ -0,0 +1,16 @@
+package com.kt.repository.delivery;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kt.entity.delivery.DeliveryAddress;
+
+public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress, Long> {
+	List<DeliveryAddress> findByUserIdAndIsActiveTrue(Long userId);
+
+	Optional<DeliveryAddress> findByUserIdAndIsDefaultTrueAndIsActiveTrue(Long userId);
+
+	Optional<DeliveryAddress> findByIdAndUserId(Long id, Long userId);
+}
