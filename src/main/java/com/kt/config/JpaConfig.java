7d1b018db089553cf1ceac8f3c88fbfc3a7ea4f6
zbass90 <smilejkh90@gmail.com>
2025-11-25 14:55:22 +0900

    feat: Jpa Config 설정

---
+++ src/main/java/com/kt/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
