1145bb4fcd769e74636600ebd301f9cbc930ec47
Josuyeon89 <138758347+Josuyeon89@users.noreply.github.com>
2025-11-26 17:24:36 +0900

    feat: JWT 기반 인증 구조 작성 (#22)
    * chore: spring security 의존성 추가
    
    * feat: @ConfigurationPropertiesScan 추가
    JwtProperties Bean으로 인식하도록 추가
    
    * feat: security 관련 에러코드 추가
    
    * feat: 토큰 생성 로직 추가
    
    * feat: JWT 인증 필터 및 예외 처리 로직 추가
    
    * feat: security configuration에 JWT 필터 등록 및 보안 설정 추가
    
    * fix: api 접근 전체 허용으로 변경

---
+++ src/main/java/com/kt/common/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.kt.common.config;
+
+import com.kt.security.JwtExceptionHandler;
+import com.kt.security.JwtFilter;
+import com.kt.security.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final JwtExceptionHandler jwtExceptionHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        JwtFilter filter = new JwtFilter(tokenProvider, jwtExceptionHandler);
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/users/signup", "/", "/auth/login", "/swagger-ui.html",
+                                "/swagger-ui/**","/api-docs/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().permitAll())
+                .logout((logout) -> logout.logoutSuccessUrl("/auth/login")
+                        .invalidateHttpSession(true))
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/kt/security/JwtFilter.java
@@ -0,0 +1,54 @@
+package com.kt.security;
+
+import com.kt.common.CustomException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    private final TokenProvider tokenProvider;
+    private final JwtExceptionHandler exceptionHandler;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String jwt = resolveToken(request);
+
+        if (StringUtils.hasText(jwt)) {
+            try {
+                if (tokenProvider.validateToken(jwt)) {
+                    Authentication authentication = tokenProvider.getAuthentication(jwt);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            } catch (CustomException e) {
+                SecurityContextHolder.clearContext();
+                exceptionHandler.handle(response, e.getErrorCode());
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(BEARER_PREFIX.length());
+        }
+        return null;
+    }
+}
