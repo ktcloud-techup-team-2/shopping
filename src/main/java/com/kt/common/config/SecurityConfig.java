ba98b66da6c356a5a45550fb7917308a1e4786a8
suyeon89 <whtndus0809@naver.com>
2025-11-27 00:16:11 +0900

    feat: swagger 토큰 헤더 생성

--- src/main/java/com/kt/common/config/SecurityConfig.java
+++ src/main/java/com/kt/common/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.kt.common.config;
 import com.kt.security.JwtExceptionHandler;
 import com.kt.security.JwtFilter;
 import com.kt.security.TokenProvider;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +18,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 
 @Configuration
 @EnableWebSecurity
@@ -44,11 +49,25 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/users/signup", "/", "/auth/login", "/swagger-ui.html",
                                 "/swagger-ui/**","/api-docs/**", "/v3/api-docs/**").permitAll()
-                        .anyRequest().permitAll())
+                        .anyRequest().authenticated())
                 .logout((logout) -> logout.logoutSuccessUrl("/auth/login")
                         .invalidateHttpSession(true))
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
     }
 }
