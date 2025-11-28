bf9adcfb9e412138f747c2ef829794e69395b239
suyeon89 <whtndus0809@naver.com>
2025-11-28 22:33:33 +0900

    feat: 회원가입 api 테스트코드 작성

---
+++ src/test/java/com/kt/controller/user/UserControllerTest.java
@@ -0,0 +1,68 @@
+package com.kt.controller.user;
+
+import com.kt.common.AbstractRestDocsTest;
+import com.kt.common.RestDocsFactory;
+import com.kt.domain.user.Gender;
+import com.kt.dto.user.UserSignUpRequest;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@ActiveProfiles("test")
+public class UserControllerTest extends AbstractRestDocsTest {
+
+    private static final String SIGNUP_URL = "/users/signup";
+
+    @Autowired
+    private RestDocsFactory restDocsFactory;
+
+    @Nested
+    class 회원가입_API {
+
+        @Test
+        void 성공 () throws Exception {
+            // given
+            UserSignUpRequest request = new UserSignUpRequest(
+                    "hihi1234",
+                    "Hihi1234!",
+                    "Hihi1234!",
+                    "JNSJ",
+                    "example123@example.com",
+                    "010-1234-1234",
+                    Gender.MALE,
+                    LocalDate.of(1999, 9, 9)
+            );
+
+            // then -> body 없이 201 created
+            mockMvc.perform(
+                    restDocsFactory.createRequest(
+                            SIGNUP_URL,
+                            request,
+                            HttpMethod.POST,
+                            objectMapper
+                    )
+            )
+                    .andExpect(status().isCreated())
+                    .andDo(
+                            restDocsFactory.success(
+                                    "user-signup",
+                                    "회원가입",
+                                    "신규 회원을 생성하는 API",
+                                    "User",
+                                    request,
+                                    null
+                            )
+                    );
+
+
+        }
+    }
+}
