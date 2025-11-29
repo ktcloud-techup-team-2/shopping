90fe383d9aa547e403cdc06e03a6703974c85f31
son_mac <smk2591@naver.com>
2025-11-29 20:57:32 +0900

    feat: 배송 이력 엔티티 구현

---
+++ src/main/java/com/kt/domain/delivery/DeliveryStatusHistory.java
@@ -0,0 +1,29 @@
+package com.kt.domain.delivery;
+
+import com.kt.common.jpa.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class DeliveryStatusHistory extends BaseTimeEntity {
+	@Column(nullable = false)
+	private Long deliverId;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private DeliveryStatus status;
+
+	public DeliveryStatusHistory create(Long deliverId, DeliveryStatus status) {
+		var history = new DeliveryStatusHistory();
+		history.deliverId = deliverId;
+		history.status = status;
+		return history;
+	}
+}
