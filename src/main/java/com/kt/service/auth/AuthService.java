2a2a25f91a3a217fb410efea76bc5c69e3b6e7d1
suyeon89 <whtndus0809@naver.com>
2025-11-30 01:47:21 +0900

    refactor: login시 refreshToken Redis에 저장

--- src/main/java/com/kt/service/auth/AuthService.java
+++ src/main/java/com/kt/service/auth/AuthService.java
@@ -1,7 +1,7 @@
 package com.kt.service.auth;
 
-import com.kt.common.api.ErrorCode;
 import com.kt.common.api.CustomException;
+import com.kt.common.api.ErrorCode;
 import com.kt.domain.user.User;
 import com.kt.dto.auth.LoginRequest;
 import com.kt.dto.auth.LoginResponse;
@@ -10,6 +10,7 @@ import com.kt.security.TokenProvider;
 import com.kt.security.dto.TokenRequestDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -17,6 +18,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.List;
 
 @Service
@@ -27,6 +29,7 @@ public class AuthService {
     private final TokenProvider tokenProvider;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RedisTemplate<String, String> redisTemplate;
 
     public LoginResponse login(LoginRequest loginRequest) {
         User user = userRepository.findByLoginId(loginRequest.loginId())
@@ -42,11 +45,18 @@ public class AuthService {
 
         TokenRequestDto tokenDto = tokenProvider.generateToken(authentication, user.getId());
 
+        String redisKey = "refreshToken:"+user.getId();
+
+        redisTemplate.opsForValue().set(
+                redisKey,
+                tokenDto.refreshToken(),
+                Duration.ofDays(7)
+        );
+
         return LoginResponse.of(
                 tokenDto.accessToken(),
                 user.getId(),
                 user.getLoginId()
         );
-
     }
 }
