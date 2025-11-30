93d81caeb2ec6e87de6afa68e70f4824f55d9198
suyeon89 <whtndus0809@naver.com>
2025-12-01 00:42:08 +0900

    feat: 내 정보 조회 테스트 코드 작성

--- src/test/java/com/kt/controller/user/UserControllerTest.java
+++ src/test/java/com/kt/controller/user/UserControllerTest.java
@@ -3,7 +3,11 @@ package com.kt.controller.user;
 import com.kt.common.AbstractRestDocsTest;
 import com.kt.common.RestDocsFactory;
 import com.kt.domain.user.Gender;
+import com.kt.domain.user.User;
+import com.kt.dto.user.UserResponse;
 import com.kt.dto.user.UserSignUpRequest;
+import com.kt.repository.user.UserRepository;
+import com.kt.security.TokenProvider;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,7 +16,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
@@ -20,9 +26,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UserControllerTest extends AbstractRestDocsTest {
 
     private static final String SIGNUP_URL = "/users/signup";
+    private static final String ME_URL = "/users/me";
 
     @Autowired
     private RestDocsFactory restDocsFactory;
+    @Autowired
+    private UserRepository userRepository;
 
     @Nested
     class 회원가입_API {
@@ -59,6 +68,37 @@ public class UserControllerTest extends AbstractRestDocsTest {
                                     "User",
                                     request,
                                     null
+                            )
+                    );
+        }
+    }
+
+    @Nested
+    class 유저_정보_조회_API {
+        @Test
+        void 성공 () throws Exception {
+            mockMvc.perform(
+                            restDocsFactory.createRequest(
+                                    "/users/me",
+                                    null,
+                                    HttpMethod.GET,
+                                    objectMapper
+                            ).with(jwtUser())
+                    )
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.id").value(1L))
+                    .andExpect(jsonPath("$.data.loginId").value("test1234"))
+                    .andExpect(jsonPath("$.data.name").value("조수연"))
+                    .andExpect(jsonPath("$.data.email").value("example123@gmail.com"))
+                    .andExpect(jsonPath("$.data.phone").value("010-1234-5678"))
+                    .andDo(
+                            restDocsFactory.success(
+                                    "users-me",
+                                    "내 정보 조회",
+                                    "현재 로그인한 사용자의 정보를 조회하는 API",
+                                    "User",
+                                    null,
+                                    UserResponse.class
                             )
                     );
         }
