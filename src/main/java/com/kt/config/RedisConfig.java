dbfa82fa13265fb57f943ca3dee9ec97ee0794a1
suyeon89 <whtndus0809@naver.com>
2025-12-01 10:56:53 +0900

    refactor: objectMapper 빈 제거

--- src/main/java/com/kt/config/RedisConfig.java
+++ src/main/java/com/kt/config/RedisConfig.java
@@ -1,7 +1,5 @@
 package com.kt.config;
 
-import com.fasterxml.jackson.core.StreamWriteConstraints;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,14 +27,5 @@ public class RedisConfig {
         StringRedisTemplate redisTemplate = new StringRedisTemplate();
         redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.getFactory().setStreamWriteConstraints(
-                StreamWriteConstraints.builder().maxNestingDepth(2000).build()
-        );
-        return objectMapper;
     }
 }
