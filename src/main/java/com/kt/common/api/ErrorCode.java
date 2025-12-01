3af10627d3fa2fffb00094016141641668e10e02
GyeongEun <wkwkuu8@gmail.com>
2025-12-01 14:20:39 +0900

    feat : 주문 조회,취소,수정 기능 구현 (#55)
    Co-authored-by: genNam <wkwku8@gmail.com>

--- src/main/java/com/kt/common/api/ErrorCode.java
+++ src/main/java/com/kt/common/api/ErrorCode.java
@@ -57,6 +57,9 @@ public enum ErrorCode {
 	PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다."),
 
 	// ---------------- ORDER_PRODUCT -------------------
+	ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "주문을 찾을 수 없습니다."),
+	ORDER_CANCEL_NOT_ALLOWED(HttpStatus.CONFLICT, "현재 상태에서는 주문을 취소할 수 없습니다."),
+	ORDER_ALREADY_CANCELLED(HttpStatus.CONFLICT, "이미 취소된 주문입니다."),
 	ORDER_PRODUCT_QUANTITY_MINIMUM(HttpStatus.BAD_REQUEST, "주문 상품 수량은 1 이상이어야 합니다.");
 
 	private final HttpStatus status;
