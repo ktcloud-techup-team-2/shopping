23e41f72d6f2344393d530da985287566c894e66
suyeon89 <whtndus0809@naver.com>
2025-12-03 23:36:46 +0900

    feat: Pet 수정 API 테스트코드 작성

--- src/test/java/com/kt/controller/pet/PetControllerTest.java
+++ src/test/java/com/kt/controller/pet/PetControllerTest.java
@@ -3,10 +3,12 @@ package com.kt.controller.pet;
 import com.kt.common.AbstractRestDocsTest;
 import com.kt.common.RestDocsFactory;
 import com.kt.domain.pet.BodyShape;
+import com.kt.domain.pet.Pet;
 import com.kt.domain.pet.PetType;
 import com.kt.domain.user.Gender;
 import com.kt.domain.user.User;
 import com.kt.dto.pet.PetRequest;
+import com.kt.repository.pet.PetRepository;
 import com.kt.repository.user.UserRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,13 +22,14 @@ import org.springframework.test.context.ActiveProfiles;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
 @ActiveProfiles("test")
 public class PetControllerTest extends AbstractRestDocsTest {
 
-    private static final String PET_CREATE_URL = "/my/pets";
+    private static final String PET_URL = "/my/pets";
 
     private static final String LOGIN_ID = "loginUser123";
     private static final String PASSWORD = "PasswordTest123!";
@@ -35,6 +38,8 @@ public class PetControllerTest extends AbstractRestDocsTest {
     private RestDocsFactory restDocsFactory;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PetRepository petRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
 
@@ -78,14 +83,60 @@ public class PetControllerTest extends AbstractRestDocsTest {
 
             mockMvc.perform(
                             restDocsFactory.createRequest(
-                                    PET_CREATE_URL,
+                                    PET_URL,
                                     request,
                                     HttpMethod.POST,
                                     objectMapper
-                            ).with(jwtUser(currentUserId))   // ← 여기!
+                            ).with(jwtUser(currentUserId))
                     )
                     .andExpect(status().isCreated());
         }
     }
 
+    @Nested
+    class 펫_수정_api {
+        @Test
+        void 성공()  throws Exception {
+            User user = userRepository.findById(currentUserId).orElseThrow();
+
+            Pet pet = Pet.create(
+                    user,
+                    PetType.DOG,
+                    "포비",
+                    Gender.MALE,
+                    true,
+                    "포메라니안",
+                    "2012-03-01",
+                    3.9,
+                    BodyShape.SKINNY,
+                    true,
+                    "https://example.com/images/pobi.jpg"
+            );
+            Long petId = petRepository.save(pet).getId();
+
+            PetRequest.Update request = new PetRequest.Update(
+                    true,
+                    4.5,
+                    BodyShape.NORMAL,
+                    true,
+                    "https://example.com/images/pobi-updated.jpg"
+            );
+
+            mockMvc.perform(
+                    restDocsFactory.createRequest(
+                            PET_URL + "/{id}",
+                            request,
+                            HttpMethod.PATCH,
+                            objectMapper,
+                            petId
+                    ).with(jwtUser(currentUserId))
+            )
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.id").value(petId))
+                    .andExpect(jsonPath("$.data.weight").value(request.weight()))
+                    .andExpect(jsonPath("$.data.neutered").value(request.neutered()))
+                    .andExpect(jsonPath("$.data.bodyShape").value(request.bodyShape().name()))
+                    .andExpect(jsonPath("$.data.allergy").value(request.allergy()));
+        }
+    }
 }
