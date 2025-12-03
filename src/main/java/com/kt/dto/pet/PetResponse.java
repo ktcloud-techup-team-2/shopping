a98ca6067addb79279490a4b5a2f0fdeb600fc80
suyeon89 <whtndus0809@naver.com>
2025-12-03 23:58:46 +0900

    refactor: response 통일

--- src/main/java/com/kt/dto/pet/PetResponse.java
+++ src/main/java/com/kt/dto/pet/PetResponse.java
@@ -4,41 +4,30 @@ import com.kt.domain.pet.BodyShape;
 import com.kt.domain.pet.Pet;
 import com.kt.domain.user.Gender;
 
-public class PetResponse {
-
-    public record Create (
-            Long id
-    ) {
-        public static Create from (Pet pet) {
-            return new Create(pet.getId());
-        }
-    }
-
-    public record Update (
-            Long id,
-            String name,
-            Gender gender,
-            Boolean neutered,
-            String breed,
-            String birthday,
-            Double weight,
-            BodyShape bodyShape,
-            boolean allergy,
-            String photoUrl
-    ) {
-        public static Update from (Pet pet) {
-            return new Update(
-                    pet.getId(),
-                    pet.getName(),
-                    pet.getGender(),
-                    pet.isNeutered(),
-                    pet.getBreed(),
-                    pet.getBirthday(),
-                    pet.getWeight(),
-                    pet.getBodyShape(),
-                    pet.isAllergy(),
-                    pet.getPhotoUrl()
-            );
-        }
+public record PetResponse (
+        Long id,
+        String name,
+        Gender gender,
+        Boolean neutered,
+        String breed,
+        String birthday,
+        Double weight,
+        BodyShape bodyShape,
+        boolean allergy,
+        String photoUrl
+) {
+    public static PetResponse from (Pet pet) {
+        return new PetResponse(
+                pet.getId(),
+                pet.getName(),
+                pet.getGender(),
+                pet.isNeutered(),
+                pet.getBreed(),
+                pet.getBirthday(),
+                pet.getWeight(),
+                pet.getBodyShape(),
+                pet.isAllergy(),
+                pet.getPhotoUrl()
+        );
     }
 }
