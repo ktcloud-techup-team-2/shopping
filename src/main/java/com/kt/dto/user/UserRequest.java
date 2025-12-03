d9b22a9999b98b46ad7c1766a6e5cf4e92fedd30
suyeon89 <whtndus0809@naver.com>
2025-12-03 10:01:15 +0900

    feat: 관리자 - 유저 비밀번호 변경 및 초기화 기능

--- src/main/java/com/kt/dto/user/UserRequest.java
+++ src/main/java/com/kt/dto/user/UserRequest.java
@@ -70,4 +70,15 @@ public class UserRequest {
             @NotBlank(message = "비밀번호 확인은 필수 입력값입니다.")
             String newPasswordConfirm
     ) {}
+
+    public record AdminPasswordChange(
+
+            @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+            @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()\\+|=])[A-Za-z\\d~!@#$%^&*()\\+|=]{8,16}$",
+                    message = "비밀번호는 영문자, 숫자, 특수문자를 모두 포함해야 합니다.")
+            String newPassword,
+
+            @NotBlank(message = "비밀번호 확인은 필수 입력값입니다.")
+            String newPasswordConfirm
+    ) {}
 }
