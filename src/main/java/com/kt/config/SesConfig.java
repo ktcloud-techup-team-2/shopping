983084996c161b1039ad1fffeceb59bda418c4b4
suyeon89 <whtndus0809@naver.com>
2025-12-10 16:25:17 +0900

    feat: ses 및 템플릿 설정 추가

---
+++ src/main/java/com/kt/config/SesConfig.java
@@ -0,0 +1,30 @@
+package com.kt.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
+
+@Configuration
+public class SesConfig {
+
+    @Value("${aws.ses.access-key}")
+    private String accessKey;
+    @Value("${aws.ses.secret-key}")
+    private String secretKey;
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public SesClient amazonSimpleEmailService() {
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return SesClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+                .build();
+    }
+}
