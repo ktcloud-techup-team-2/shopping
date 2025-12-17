6ea96834b945a2ac71a9cae0f4cd74b5b9264499
suyeon89 <whtndus0809@naver.com>
2025-12-17 22:56:33 +0900

    refactor: 아이디 찾기 요청/응답 dto 생성 - controller 수정

--- src/main/java/com/kt/controller/email/EmailController.java
+++ src/main/java/com/kt/controller/email/EmailController.java
@@ -1,9 +1,8 @@
 package com.kt.controller.email;
 
 import com.kt.common.api.ApiResponseEntity;
-import com.kt.dto.auth.EmailRequest;
-import com.kt.dto.auth.EmailResponse;
-import com.kt.dto.auth.EmailVerificationRequest;
+import com.kt.dto.email.EmailResponse;
+import com.kt.dto.email.EmailRequest;
 import com.kt.service.email.EmailService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,13 +16,13 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping("/auth-email")
-    public ApiResponseEntity<EmailResponse> sendAuthEmail(@RequestBody @Valid EmailRequest request) {
-        EmailResponse response = emailService.sendEmail(request.email());
+    public ApiResponseEntity<EmailResponse.AuthenticationResponse> sendAuthEmail(@RequestBody @Valid EmailRequest.VerificationRequest request) {
+        EmailResponse.AuthenticationResponse response = emailService.sendEmail(request.email());
         return ApiResponseEntity.success(response);
     }
 
-    @GetMapping
-    public ApiResponseEntity<Boolean> verifyCode(@RequestBody @Valid EmailVerificationRequest request) {
+    @PostMapping
+    public ApiResponseEntity<Boolean> verifyCode(@RequestBody @Valid EmailRequest.VerificationConfirmRequest request) {
         boolean result = emailService.verifyCode(request.email(), request.code());
         return ApiResponseEntity.success(result);
     }
