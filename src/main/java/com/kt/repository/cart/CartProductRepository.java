75926a9d043d3ad21afe16099f04c1b876568987
genNam <wkwku8@gmail.com>
2025-12-22 00:49:24 +0900

    refactor: CartProductRepository 상속으로 수정

--- src/main/java/com/kt/repository/cart/CartProductRepository.java
+++ src/main/java/com/kt/repository/cart/CartProductRepository.java
@@ -8,7 +8,7 @@ import com.kt.domain.cartproduct.CartProduct;
 import com.kt.repository.category.CategoryRepositoryCustom;
 
 //JPA가 제공하는 기본 메서드를 사용하는 메인 Repository
-public interface CartProductRepository extends JpaRepository<CartProduct,Long>, CategoryRepositoryCustom {
+public interface CartProductRepository extends JpaRepository<CartProduct,Long>, CartProductRepositoryCustom {
 
 	//장바구니에 들어갈 상품을 저장하거나 조회하는 역할
 	Optional<CartProduct> findByCartIdAndProductId(Long cartId, Long ProductId);
