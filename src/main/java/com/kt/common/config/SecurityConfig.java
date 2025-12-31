9f9cfe1cb247761c3945bcee992cf2b0821c526f
suyeon89 <whtndus0809@naver.com>
2025-12-31 17:11:04 +0900

    refactor: security 충돌 해결

--- src/main/java/com/kt/common/config/SecurityConfig.java
+++ src/main/java/com/kt/common/config/SecurityConfig.java
@@ -57,12 +57,6 @@ public class SecurityConfig {
                         .requestMatchers("/users/signup", "/", "/auth/login","/auth/reissue","/swagger-ui.html",
                                 "/swagger-ui/**","/api-docs/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/super-admin/**").hasRole("SUPER_ADMIN")
-                        .requestMatchers("/users/signup", "/admins/signup", "/", "/auth/login", "/auth/reissue",
-                                "/auth/reset-password/request", "/auth/reset-password/verify", "/auth/update-password", "/auth/find-id",
-                                "/swagger-ui.html", "/swagger-ui/**","/api-docs/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/users/signup", "/", "/auth/login","/auth/reissue","/swagger-ui.html",
-                                "/swagger-ui/**","/api-docs/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/super-admin/**").hasRole("SUPER_ADMIN")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
