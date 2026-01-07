28b7de55b2877b8ed4774fd9b7a30b1bd3f73287
Son Moo Kyung <88808478+son2304@users.noreply.github.com>
2026-01-07 15:21:22 +0900

    feat: 파일 시스템 구현 (#160)
    * feat: 파일(이미지) 업로드 시스템 구현, 파일 관련 에러코드 추가, yml파일 설정 추가
    
    * feat: 게시판과 파일 업로드 시스템 연동
    
    * feat: jpg 확장자로 포맷 변환

---
+++ src/main/java/com/kt/service/file/S3Service.java
@@ -0,0 +1,94 @@
+package com.kt.service.file;
+
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Template;
+import lombok.RequiredArgsConstructor;
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
+
+import com.kt.common.api.CustomException;
+import com.kt.common.api.ErrorCode;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+	private final S3Template s3Template;
+
+	@Value("${spring.cloud.aws.s3.bucket}")
+	private String bucket;
+
+	@Value("${spring.cloud.aws.region.static}")
+	private String region;
+
+	@Value("${spring.servlet.multipart.max-file-size}")
+	private DataSize maxFileSize;
+
+	public List<String> uploadFiles(List<MultipartFile> files) {
+		List<String> uploadedUrls = new ArrayList<>();
+
+		if (files == null || files.isEmpty()) {
+			return uploadedUrls;
+		}
+
+		for (MultipartFile file : files) {
+			if (file.isEmpty()) continue;
+
+			validateImageFile(file);
+
+			String originalFileName = file.getOriginalFilename();
+			String uuidFileName = UUID.randomUUID().toString() + ".jpg";
+
+			try {
+				ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+				Thumbnails.of(file.getInputStream())
+					.size(1000, 1000)
+					.outputFormat("jpg")
+					.outputQuality(0.8)
+					.toOutputStream(os);
+
+				byte[] buffer = os.toByteArray();
+				InputStream is = new ByteArrayInputStream(buffer);
+
+				ObjectMetadata metadata = ObjectMetadata.builder()
+					.contentType("image/jpeg")
+					.build();
+
+				s3Template.upload(bucket, uuidFileName, is, metadata);
+
+				String fileUrl = String.format("https://%s.s3.%s.amazonaws.com/%s", bucket, region, uuidFileName);
+				uploadedUrls.add(fileUrl);
+
+			} catch (Exception e) {
+				throw new CustomException(ErrorCode.FILE_UPLOAD_FAILED);
+			}
+		}
+
+		return uploadedUrls;
+	}
+
+	private void validateImageFile(MultipartFile file) {
+		if (file.getSize() > maxFileSize.toBytes()) {
+			throw new CustomException(ErrorCode.FILE_SIZE_EXCEEDED);
+		}
+
+		String contentType = file.getContentType();
+		if (contentType == null || !contentType.startsWith("image")) {
+			throw new CustomException(ErrorCode.FILE_TYPE_NOT_SUPPORTED);
+		}
+	}
+}
